78a6b347f70c006c5a219c8925887751b50eb0f3
Jessica Mulein <jessica@digitaldefiance.org>
2025-12-14 19:38:18 -0800

    v2.9.11

--- jest.config.js
+++ jest.config.js
@@ -8,27 +8,42 @@ module.exports = {
   },
   maxWorkers: 4,
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/packages/digitaldefiance-express-suite-react-components',
+  coverageDirectory:
+    '../../coverage/packages/digitaldefiance-express-suite-react-components',
   setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'],
   transform: {
-    '^.+\\.tsx?$': ['ts-jest', {
-      tsconfig: {
-        jsx: 'react-jsx',
-        esModuleInterop: true,
-        allowSyntheticDefaultImports: true,
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+        },
       },
-    }],
+    ],
+    '^.+\\.jsx?$': [
+      'ts-jest',
+      {
+        tsconfig: {
+          jsx: 'react-jsx',
+          esModuleInterop: true,
+          allowSyntheticDefaultImports: true,
+        },
+      },
+    ],
   },
   moduleNameMapper: {
-    '^@digitaldefiance/ecies-lib$': '<rootDir>/../digitaldefiance-ecies-lib/src/index.ts',
-    '^@digitaldefiance/i18n-lib$': '<rootDir>/../digitaldefiance-i18n-lib/src/index.ts',
-    '^@digitaldefiance/suite-core-lib$': '<rootDir>/../digitaldefiance-suite-core-lib/src/index.ts',
-    '^bson$': '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
-    '^@scure/bip32$': '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
-    '^@noble/curves/(.*)$': '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
-    '^@noble/hashes/(.*)$': '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
-    '^ethereum-cryptography/(.*)$': '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
-    '^@ethereumjs/util$': '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
-    '^@ethereumjs/wallet$': '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
+    '^@digitaldefiance/ecies-lib$':
+      '<rootDir>/../digitaldefiance-ecies-lib/src/index.ts',
+    '^@digitaldefiance/i18n-lib$':
+      '<rootDir>/../digitaldefiance-i18n-lib/src/index.ts',
+    '^@digitaldefiance/suite-core-lib$':
+      '<rootDir>/../digitaldefiance-suite-core-lib/src/index.ts',
+    '^bson$':
+      '<rootDir>/../digitaldefiance-express-suite-test-utils/src/lib/bson-mock.ts',
   },
+  transformIgnorePatterns: [
+    'node_modules/(?!(@noble|@scure|ethereum-cryptography|@ethereumjs)/)',
+  ],
 };
