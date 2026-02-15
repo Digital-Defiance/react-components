55ea0ad0dd8b528581051d25bb6e4c9d8bd20d55
Jessica Mulein <jessica@digitaldefiance.org>
2026-02-14 16:56:58 -0800

    v2.13.14

--- src/components/TranslatedTitle.tsx
+++ src/components/TranslatedTitle.tsx
@@ -6,11 +6,13 @@ import { useI18n } from '../contexts';
 interface FCParams<TEnum extends string> {
   componentId: string;
   stringKey: TEnum;
+  vars?: Record<string, string | number>;
 }
 
 export const TranslatedTitle = <TEnum extends string>({
   componentId,
   stringKey,
+  vars,
 }: FCParams<TEnum>): null => {
   const { tComponent, currentLanguage } = useI18n();
 
@@ -18,10 +20,10 @@ export const TranslatedTitle = <TEnum extends string>({
     document.title = tComponent<TEnum>(
       componentId,
       stringKey,
-      undefined,
+      vars,
       currentLanguage
     );
-  }, [tComponent, componentId, stringKey, currentLanguage]);
+  }, [tComponent, componentId, stringKey, vars, currentLanguage]);
 
   return null;
 };
